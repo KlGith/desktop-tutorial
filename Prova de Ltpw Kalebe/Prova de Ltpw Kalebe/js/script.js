1294ac3d884485550024330c56f8870faf1cd501
CARDOSOCRIS <92881122+CARDOSOCRIS@users.noreply.github.com>
2022-05-03 09:53:22 -0300

    prova ltpw

---
+++ Prova de Ltpw Kalebe/Prova de Ltpw Kalebe/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('total').value = valor;
+
+    document.getElementById('bacon').checked = false;
+    document.getElementById('personagens').checked = false;
+    document.getElementById('atrações').checked = false;
+    document.getElementById('maquiagemInfantil').checked = false;
+    document.getElementById('brindes').checked = false;
